c64d7138b74cef64c07cc069b8abe6867dc47fb5
ngkeanchewet <ngkeanchewet@gmail.com>
2022-11-03 09:22:56 +0800

    fix the latest meeting date

--- irpr/app.py
+++ irpr/app.py
@@ -3,22 +3,21 @@ import pandas as pd
 from irpr import *
 
 try:
-    df = pd.read_csv('data.csv')
-    ldf = pd.read_csv('ldata.csv')
+    df = pd.read_csv("data.csv")
+    ldf = pd.read_csv("ldata.csv")
 except:
-    df = pd.read_csv('irpr/data.csv')
-    ldf = pd.read_csv('irpr/ldata.csv')
+    df = pd.read_csv("irpr/data.csv")
+    ldf = pd.read_csv("irpr/ldata.csv")
 
 st.set_page_config(
-    page_title="Interest Rate Probability | US Federal Reserve",
-    layout="wide",
+    page_title="Interest Rate Probability | US Federal Reserve", layout="wide",
 )
 st.title("Interest Rate Probability | US Federal Reserve")
 
-#st.table(get_table(ldf))
+# st.table(get_table(ldf))
 st.write(get_table(ldf).to_html(escape=False, index=False), unsafe_allow_html=True)
 
-dates = sorted(df['GV1_DATE'].unique())
+dates = sorted(ldf["GV1_DATE"].unique())
 figs = [get_prob_line_by_date(df, d)[0] for d in dates[:4]]
 fig_line = get_expected_line(df)
 st.plotly_chart(fig_line)
